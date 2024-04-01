e5896c02355a88b9e0cebd30f7bb53ff19a94945
abdissa-jpeg <159719786+abdissa-jpeg@users.noreply.github.com>
2024-04-01 17:45:15 +0300

    First commit in this new account

---
+++ first.py
@@ -0,0 +1,9 @@
+def first(num):
+    print(num)
+    while num!=1:
+        if num%2:
+            num=3*num+1
+        else:
+            num=num//2
+        print(num)
+first(198767543245678909876543567890876543567898765434567887654343567898876543456789765434567898)
